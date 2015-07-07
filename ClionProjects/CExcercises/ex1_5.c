d77899f1413bf65663c9a7f671bd85b1fa4e2edf
Ibtissam <ik@karouach.se>
2015-07-07 16:55:40 +0200

    Ex1.5

---
+++ ClionProjects/CExcercises/ex1_5.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : ex1_4.c
+ Author      :
+ Question    : Write a program to print the corresponding
+               Celsius to Fahrenheit table.
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int fahrn;
+    for (fahrn = 300; fahrn >= 0; fahrn = fahrn - 20)
+         printf("%3d %6.1f\n", fahrn, (5.0 / 9.0)*(fahrn - 32));
+    return EXIT_SUCCESS;
+}
