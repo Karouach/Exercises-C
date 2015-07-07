7a0e8ce31d4d2fff11c6e090ecd595ba0e07bdfb
Ibtissam <ik@karouach.se>
2015-07-07 17:04:11 +0200

    Exercise 1.5 - Macros

--- ClionProjects/CExcercises/ex1_5_1.c
+++ ClionProjects/CExcercises/ex1_5_1.c
@@ -1,19 +1,23 @@
 /*
  ============================================================================
- Name        : ex1_5.c
+ Name        : ex1_5_1.c
  Author      :
  Question    : Write a program to print the corresponding
-               Celsius to Fahrenheit table.
+               Celsius to Fahrenheit table with symbolic constants
  ============================================================================
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
+#define LOWER 0
+#define LARGER 300
+#define STEP 20
+
 int main()
 {
     int fahrn;
-    for (fahrn = 300; fahrn >= 0; fahrn = fahrn - 20)
+    for (fahrn = LARGER; fahrn >= LOWER; fahrn = fahrn - STEP)
          printf("%3d %6.1f\n", fahrn, (5.0 / 9.0)*(fahrn - 32));
     return EXIT_SUCCESS;
 }
